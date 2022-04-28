bdbdab8d29de3bf60b7ff79c91186c04c1b9dd2f
Chiranjeevi Tirunagari <vchiranjeeviak.tirunagari@gmail.com>
2022-04-29 02:43:27 +0530

    Added comments to stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,22 +1,22 @@
-class stack:
-    stk = []  # Empty stack initialization
-    top = -1  # Intializing top as -1
+class Stack:
 
     def __init__(self, size):
+        self.stack = []  # Empty stack initialization
+        self.top = -1  # Intializing top as -1
         self.size = size  # Getting size of the stack while creating object
 
     def isFull(self):
         # return true if size is equal to current length
-        return len(self.stk) == self.size
+        return len(self.stack) == self.size
 
     def isEmpty(self):
-        return len(self.stk) == 0  # return true if current length is 0
+        return len(self.stack) == 0  # return true if current length is 0
 
     def push(self, item):  # if stack is not full, push append an item
         if(self.isFull()):
             print("Stack is full")
             return
-        self.stk.append(item)
+        self.stack.append(item)  # O(1)
         self.top += 1  # increment top after pushing
         self.peek()
         return
@@ -25,7 +25,7 @@ class stack:
         if(self.isEmpty()):
             print("Stack is empty")
             return
-        popped_item = self.stk.pop()
+        popped_item = self.stack.pop()  # O(1)
         self.top -= 1  # decrement top after popping
         self.peek()
         return popped_item
@@ -34,10 +34,10 @@ class stack:
         if(self.isEmpty()):
             print("Stack is empty")
             return
-        print(self.stk[len(self.stk)-1])
+        print(self.stack[len(self.stack)-1])
 
 
-stack_obj = stack(5)
+stack_obj = Stack(5)
 stack_obj.pop()
 stack_obj.push(1)
 stack_obj.peek()
